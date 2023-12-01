d2a2818fc2ccdc094aefa3971f65384fed60272e
Drew Hamilton <drewh@squareup.com>
2023-12-01 14:58:00 -0800

    Demonstrate animateContentSize bug in sample

--- sample/src/test/java/app/cash/paparazzi/sample/ComposeA11yTest.kt
+++ sample/src/test/java/app/cash/paparazzi/sample/ComposeA11yTest.kt
@@ -1,10 +1,13 @@
 package app.cash.paparazzi.sample
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material.Checkbox
@@ -13,21 +16,36 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.InstantAnimationsRule
 import app.cash.paparazzi.Paparazzi
 import app.cash.paparazzi.accessibility.AccessibilityRenderExtension
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class ComposeA11yTest {
+@RunWith(TestParameterInjector::class)
+class ComposeA11yTest(
+  @TestParameter renderAccessibilityInfo: Boolean,
+) {
   @get:Rule
   val paparazzi = Paparazzi(
-    deviceConfig = DeviceConfig.PIXEL.copy(screenWidth = DeviceConfig.PIXEL.screenWidth * 2, softButtons = false),
-    renderExtensions = setOf(AccessibilityRenderExtension())
+    deviceConfig = DeviceConfig.PIXEL.copy(
+      screenWidth = DeviceConfig.PIXEL.screenWidth * (if (renderAccessibilityInfo) 2 else 1),
+      softButtons = false,
+    ),
+    renderExtensions = if (renderAccessibilityInfo) setOf(AccessibilityRenderExtension()) else emptySet()
   )
+
+  @get:Rule
+  val instantAnimationsRule = InstantAnimationsRule()
 
   @Test
   fun compositeItems() {
@@ -71,6 +89,20 @@ class ComposeA11yTest {
           }
         }
       }
+    }
+  }
+
+  @Test
+  fun animateContentSize() {
+    paparazzi.snapshot {
+      Text(
+        text = "animateContentSize modifier is used",
+        modifier = Modifier
+          .fillMaxSize()
+          .animateContentSize()
+          .background(Color.LightGray),
+        textAlign = TextAlign.End,
+      )
     }
   }
 }
